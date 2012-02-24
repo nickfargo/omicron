e5d070a6201857a2593f05bfb4fd1951d170a332
Nick Fargo <nick.fargo@gmail.com>
2012-02-24 07:06:02 -0800

    add "delta composition" tests

--- test/unit/extend.test.js
+++ test/unit/extend.test.js
@@ -32,6 +32,57 @@ test( "referential invariance", function () {
 	assert.strictEqual( delta.ref.ref, deep, "Reference held by `delta` after deletion from `object`" );
 	assert.deepEqual( Z.extend( 'deep', object, delta ), original, "Restored `object` resembles original state" );
 	assert.strictEqual( object.ref.ref, deep, "Reference held by `object` restored" );
+test( "delta composition", function () {
+    var _ = undefined, DELETE = Z.DELETE,
+
+        original = {
+            a: 1,
+            b: '2',
+            c: {
+                d: 3,
+                e: 4
+            },
+            g: [ 0, 1 ]
+        },
+        edits = [
+            { a: "uno" },
+            { a: "un", b: "deux" },
+            { c: { d: "III", e: "IV" } },
+            { a: DELETE, f: "Foo" },
+            { b: "dos", g: [ _, "une" ] }
+        ],
+        object = Z.extend( true, {}, original ),
+        deltas = Z.extend.apply( Z, [ 'deep delta', object ].concat( edits ) );
+
+    assert.deepEqual(
+        object,
+        {
+            b: "dos",
+            c: {
+                d: "III",
+                e: "IV"
+            },
+            f: "Foo",
+            g: [ 0, "une" ]
+        },
+        "Sequential edits"
+    );
+    assert.deepEqual(
+        deltas,
+        [
+            { a: 1 },
+            { a: "uno", b: "2" },
+            { c: { d: 3, e: 4 } },
+            { a: "un", f: DELETE },
+            { b: "deux", g: [ _, 1 ] }
+        ],
+        "Delta sequence"
+    );
+    assert.deepEqual(
+        Z.extend.apply( Z, [ true, object ].concat( deltas.reverse() ) ),
+        original,
+        "Delta composition applied in reverse yields content-equivalence to the original object"
+    );
 });
 
 })( QUnit );
