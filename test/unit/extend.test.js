0cf632f7c9f7127efb9856aafc2c4d4a4888f135
Nick Fargo <nick.fargo@gmail.com>
2012-02-24 07:06:23 -0800

    reformat invariance tests

--- test/unit/extend.test.js
+++ test/unit/extend.test.js
@@ -3,35 +3,87 @@
 module( "extend" );
 
 test( "structural invariance", function () {
-	var	_ = undefined, DEL = Z.DELETE,
+    var _ = undefined, DELETE = Z.DELETE,
 
-		original = { a:1, b:2, c:{ d:3, e:4 }, f:[ 1, 1, 2, 3, 5, 8 ] },
-		object = Z.extend( true, {}, original ),
-		edit = { a:DEL, b:'2', c:{ d:DEL }, f:[ 0, _, _, 4, 8, 16 ] },
-		delta = Z.extend( 'deep delta', object, edit );
-	
-	assert.deepEqual( delta, { a:1, b:2, c:{ d:3 }, f:[ 1, _, _, 3, 5, 8 ] }, "delta" );
-	assert.deepEqual( object, { b:'2', c:{ e:4 }, f:[ 0, 1, 2, 4, 8, 16 ] }, "object" );
-	assert.deepEqual( Z.extend( 'deep', object, delta ), original, "object restored from delta to original state" );
+        original = {
+            a: 1,
+            b: 2,
+            c: {
+                d: 3,
+                e: 4
+            },
+            g: [ 1, 1, 2, 3, 5, 8 ]
+        },
+        edit = {
+            a: DELETE,
+            b: '2',
+            c: {
+                d: DELETE,
+                e: 4,
+                f: ['foo']
+            },
+            g: [ 0, _, _, 4, 8, 16 ],
+            h: 'bar'
+        },
+        object = Z.extend( true, {}, original ),
+        delta = Z.extend( 'deep delta', object, edit );
+
+    assert.deepEqual( delta, {
+        a: 1,
+        b: 2,
+        c: {
+            d: 3,
+            f: DELETE
+        },
+        g: [ 1, _, _, 3, 5, 8 ],
+        h: DELETE
+    });
+    assert.deepEqual( object, {
+        b: '2',
+        c: {
+            e: 4,
+            f: ['foo']
+        },
+        g: [ 0, 1, 2, 4, 8, 16 ],
+        h: 'bar'
+    });
+    assert.deepEqual( Z.extend( true, object, delta ), original, "object restored from delta to original state" );
 });
 
 test( "referential invariance", function () {
-	function Class ( properties ) {
-		properties && Z.extend( true, this, properties );
-	}
-	
-	var	_ = undefined, DEL = Z.DELETE,
+    function Class ( properties ) {
+        properties && Z.extend( true, this, properties );
+    }
+    
+    var _ = undefined, DELETE = Z.DELETE,
+        fn = function () { return 3; },
 
-		deep = new Class({ d:4, e:5 }),
-		shallow = new Class({ a:1, b:2, ref:deep }),
-		original = { ref:shallow, c:3 },
-		object = Z.extend( true, {}, original ),
-		edit = { ref:{ ref:DEL } },
-		delta = Z.extend( 'deep delta', object, edit );
-	
-	assert.strictEqual( delta.ref.ref, deep, "Reference held by `delta` after deletion from `object`" );
-	assert.deepEqual( Z.extend( 'deep', object, delta ), original, "Restored `object` resembles original state" );
-	assert.strictEqual( object.ref.ref, deep, "Reference held by `object` restored" );
+        deep = new Class({
+            d: 4,
+            e: 5
+        }),
+        shallow = new Class({
+            a: 1,
+            b: 2,
+            ref: deep
+        }),
+        original = {
+            ref: shallow,
+            c: fn
+        },
+        edit = {
+            ref: {
+                ref: DELETE
+            }
+        },
+        object = Z.extend( true, {}, original ),
+        delta = Z.extend( 'deep delta', object, edit );
+    
+    assert.strictEqual( delta.ref.ref, deep, "Reference held by `delta` after deletion from `object`" );
+    assert.deepEqual( Z.extend( 'deep', object, delta ), original, "Restored `object` resembles original state" );
+    assert.strictEqual( object.ref.ref, deep, "Reference held by `object` restored" );
+});
+
 test( "delta composition", function () {
     var _ = undefined, DELETE = Z.DELETE,
 
